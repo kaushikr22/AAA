993b073a6f5dfa6108bf0de92bf741f9431d897e
kaushik <kaushikr.apcsa2@gmail.com>
2023-11-21 12:51:23 -0800

    yrdtfj

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <body>
+        <h1>hello madharchod</h1>
+      </body>
+    </html>
+  );
+}
+
+export default App;
